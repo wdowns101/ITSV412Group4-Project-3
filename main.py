a4806539fb5526eff7d9770083cdd7d8b1207516
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-07 13:01:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import os
+import requests
 
+# Define the URL of the file to download
+file_url = "https://www.test.com/examplefile.txt"
+
+# Define the path to the "Downloads" folder
+downloads_folder = os.path.expanduser("~") + "/Downloads/"
