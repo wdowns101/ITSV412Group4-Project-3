15dad4d4b6eeb1d847249773a8b64a5b3fe63c94
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-07 15:34:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import requests
 
-# Define the URL of the file to download
-file_url = "https://s3.amazonaws.com/tcmg476/http_access_log"
-req = requests.get(file_url)
-filename = req.url[file_url.rfind('/')+1:]
-open(filename, 'wb') as f:
+downloadUrl = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
-# Define the path to the "Downloads" folder
-downloads_folder = os.path.expanduser("~") + "Users/Downloads/"
+req = requests.get(downloadUrl)
+filename = req.url[downloadUrl.rfind('/')+1:]
+
+with open(filename, 'wb') as f:
+    for chunk in req.iter_content(chunk_size=8192):
+        if chunk:
+            f.write(chunk)
+
+def download_file(url, filename=''):
+    try:
+        if filename:
+            pass            
+        else:
+            filename = req.url[downloadUrl.rfind('/')+1:]
+
+        with requests.get(url) as req:
+            with open(filename, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+            return filename
+    except Exception as e:
+        print(e)
+        return None
