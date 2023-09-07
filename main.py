a3428cc33d07b3c0143b8d228e5bf2e4871b2f5e
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-07 16:12:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ import os
 import os.path
 import requests
 
-downloadUrl = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+path = './http_access_log'
 
-req = requests.get(downloadUrl)
-filename = req.url[downloadUrl.rfind('/')+1:]
+check_file = os.path.isfile(path)
 
-with open(filename, 'wb') as f:
-    for chunk in req.iter_content(chunk_size=8192):
-        if chunk:
-            f.write(chunk)
+print(check_file)
 
-def download_file(url, filename=''):
-    try:
-        if filename:
-            pass            
-        else:
-            filename = req.url[downloadUrl.rfind('/')+1:]
+while (check_file == False):
+    print("Downloading the file!")
+    downloadUrl = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
-        with requests.get(url) as req:
-            with open(filename, 'wb') as f:
-                for chunk in req.iter_content(chunk_size=8192):
-                    if chunk:
-                        f.write(chunk)
-            return filename
-    except Exception as e:
-        print(e)
-        return None
+    req = requests.get(downloadUrl)
+    filename = req.url[downloadUrl.rfind('/')+1:]
+
+    with open(filename, 'wb') as f:
+        for chunk in req.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+
+    def download_file(url, filename=''):
+        try:
+            if filename:
+                pass            
+            else:
+                filename = req.url[downloadUrl.rfind('/')+1:]
+
+            with requests.get(url) as req:
+                with open(filename, 'wb') as f:
+                    for chunk in req.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+                return filename
+        except Exception as e:
+            print(e)
+            return None
+        print("File has been downloaded!")
+    continue 
+
+print("you have the file!")
