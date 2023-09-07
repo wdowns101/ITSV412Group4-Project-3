414aa1c6ec110669ec2cb4f6e1ba058dfd9db218
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-07 14:30:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 # Define the URL of the file to download
-file_url = "https://www.test.com/examplefile.txt"
+file_url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 
 # Define the path to the "Downloads" folder
-downloads_folder = os.path.expanduser("~") + "/Downloads/"
+downloads_folder = os.path.expanduser("~") + "Users/Downloads/"
