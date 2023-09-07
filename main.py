1fc512bc661d487c4e94a02aa5a49cff5b98a3d1
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-07 14:54:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 # Define the URL of the file to download
 file_url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 req = request.get(file_url)
-filename = req.url[file_url.rfind('/')+1:0]
-with open(filename, 'wb') as f:
+filename = req.url[file_url.rfind('/')+1:]
+open(filename, 'wb') as f:
 
 # Define the path to the "Downloads" folder
 downloads_folder = os.path.expanduser("~") + "Users/Downloads/"
