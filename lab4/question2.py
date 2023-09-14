49cff9c614fc534146db4240a9df28f39dfee8ad
alyssianav <143128408+alyssianav@users.noreply.github.com>
2023-09-14 15:35:28 -0500

    Update question2.py

--- lab4/question2.py
+++ lab4/question2.py
@@ -5,3 +5,12 @@ local_file
 path = './http_access_log.txt'
 
 weekly_request_counts = {}
+
+# Open the log file and read its contents line by line
+with open(path, 'r') as log_file:
+    for line in log_file:
+        # Split the log entry by space
+        parts = line.split()
+        if len(parts) >= 10:
+            # Extract the date from the log entry 
+            date_str = parts[3][1:12]  #
