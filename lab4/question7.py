0530cad3c81628945e4726de21d6269cf468557e
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-14 15:27:22 -0500

    Update question7.py

--- lab4/question7.py
+++ lab4/question7.py
@@ -1,1 +1,30 @@
+import os
+import os.path
+from urllib.request import  urlretrieve #use url.lib
+
+
+path = './http_access_log.txt'
+
+check_file = os.path.isfile(path) #this is a boolean statement
+
+if (check_file == False):
+
+    print("You do not have the file.") #add fetching file
+    print("Downloading the file...")
+    #The file we want to download
+    downloadUrl = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    local_file = 'http_access_log.txt'
+
+    local_file, headers = urlretrieve(downloadUrl,local_file)
+    
+    print("File has been downloaded!")
+
+    
+#Verifies that you have the file and will begin analyzing the data
+print("You have the file!")
+print("Analyzing data...")
+with open('http_access_log.txt','r') as file:
+    li = file.readlines()
+
+
 
