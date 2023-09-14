26478690040a1f12561fd8b75276d537f19c08cd
wdowns101 <143128973+wdowns101@users.noreply.github.com>
2023-09-14 15:26:13 -0500

    Update mainV2.py

--- mainV2.py
+++ mainV2.py
@@ -1,67 +1,30 @@
 import os
 import os.path
-import requests #use url.lib
+from urllib.request import  urlretrieve #use url.lib
 
-'''REMOTE_URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-local_file
-'''
+
 path = './http_access_log.txt'
 
 check_file = os.path.isfile(path) #this is a boolean statement
 
 if (check_file == False):
 
-    
     print("You do not have the file.") #add fetching file
-'''
-    # Fetch the file from the remote server and save it to disk
-    print("\n\nDownloading log file from URI... ")
-    response = urllib2.urlopen(REMOTE_URL)
-    with open(LOCAL_FILE, "wb") as local:
-	local.write(response.read())
-print "File retrieved and saved to disk (%s) \n\n" % LOCAL_FILE
-#else statement here to move on and parse the data
-'''
-while (check_file != True): #while statement can be replaced with url.lib 
-
     print("Downloading the file...")
     #The file we want to download
     downloadUrl = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    local_file = 'http_access_log.txt'
 
-    req = requests.get(downloadUrl)
-    filename = "http_access_log.txt"
-    #Downloads the file as a .txt file
-    with open(filename, 'wb') as textfile:
-        for chunk in req.iter_content(chunk_size=8192):
-            if chunk:
-                textfile.write(chunk)
+    local_file, headers = urlretrieve(downloadUrl,local_file)
+    
+    print("File has been downloaded!")
 
-    def download_file(url, filename=''): #Function that had to be added for this to work for some reason
-        try:
-            if filename:
-                pass            
-            else:
-                filename = req.url[downloadUrl.rfind('/')+1:]
- 
-            with requests.get(url) as req:
-                with open(filename, 'wb') as textfile:
-                    for chunk in req.iter_content(chunk_size=8192):
-                        if chunk:
-                            textfile.write(chunk)
-                return filename
-        except Exception as e:
-            print(e)
-            return None
-        print("File has been downloaded!")
-    #Looks for the file to close the wile loop
-    check_file = os.path.isfile(path)
-    print(check_file)
-
+    
 #Verifies that you have the file and will begin analyzing the data
 print("You have the file!")
 print("Analyzing data...")
 with open('http_access_log.txt','r') as file:
-   li = file.readlines()
+    li = file.readlines()
 # Define the criteria
 criteria = ["May/1995", "Jun/1995", "Jul/1995", "Aug/1995", "Sep/1995", "Oct/1995"]
 
