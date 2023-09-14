78dcfd721d36ba36637b18ffe6a39f4500c14347
alyssianav <143128408+alyssianav@users.noreply.github.com>
2023-09-14 15:34:03 -0500

    Update and rename question2 to question2.py

---
+++ lab4/question2.py
@@ -0,0 +1,7 @@
+#How many requests were made on a week-by-week basis? Per month?
+'''REMOTE_URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+local_file
+'''
+path = './http_access_log.txt'
+
+weekly_request_counts = {}
