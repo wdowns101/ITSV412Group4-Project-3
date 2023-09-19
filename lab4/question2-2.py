48e5810f78ffe258e87a8387c892347154c2b842
alyssianav <143128408+alyssianav@users.noreply.github.com>
2023-09-19 12:09:50 -0500

    Update question2-2.py

--- lab4/question2-2.py
+++ lab4/question2-2.py
@@ -1,15 +1,14 @@
 #How many requests were made on a monthly basis?
+from datetime import datetime
 import os
 import os.path
-import datetime
-from urllib.request import  urlretrieve #use url.lib
-
+from urllib.request import urlretrieve  #use url.lib
 
 path = './http_access_log.txt'
 
-check_file = os.path.isfile(path) #this is a boolean statement
+check_file = os.path.isfile(path) 
 
-if (check_file == False):
+if (check_file is False):
 
     print("You do not have the file.") #add fetching file
     print("Downloading the file...")
@@ -21,30 +20,37 @@ if (check_file == False):
     
     print("File has been downloaded!")
 
-    
-#Verifies that you have the file and will begin analyzing the data
+    #Verifies that you have the file and will begin analyzing the data
 print("You have the file!")
 print("Analyzing data...")
 with open('http_access_log.txt','r') as file:
     li = file.readlines()
 
-# Initialize a dictionary to store the monthly request counts
+#Store monthly requests
 monthly_request_counts = {}
 
-# Open the log file and read its contents line by line
 with open(path, 'r') as log_file:
     for line in log_file:
         # Split the log entry by space
         parts = line.split()
         if len(parts) >= 10:
-            # Extract the date from the log entry 
-            date_str = parts[3][1:12]  
-          
-            if date_str in monthly_request_counts:
-                monthly_request_counts[date_str] += 1
-            else:
-                monthly_request_counts[date_str] = 1
+            # Extract the date from the log entry
+            date_str = parts[3][1:12] 
 
-# Print the daily request counts
-for date, count in sorted(monthly_request_counts.items()):
-    print(f'{date}: {count} requests')
+            # Parse the date string into a datetime object
+            log_date = datetime.strptime(date_str, '%d/%b/%Y')
+
+            # Calculate the year and month for the date
+            year_month = (log_date.year, log_date.month)
+
+            # Increment the request count for the extracted month
+            if year_month in monthly_request_counts:
+                monthly_request_counts[year_month] += 1
+            else:
+                monthly_request_counts[year_month] = 1
+
+# Print the monthly request counts
+for year_month, count in sorted(monthly_request_counts.items()):
+    year, month = year_month
+    print(f'{year}-{month:02d}: {count} requests')
+  
